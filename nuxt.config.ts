bc1f4fed58f16807471ca8752f60566916d52836
Yasunori Kobayashi <52128219+morimorimokenpi@users.noreply.github.com>
2023-12-12 01:35:55 +0900

    :sparkles: Add vuetify

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,32 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import vuetify from 'vite-plugin-vuetify'
+
+const srcDir = 'src/'
 export default defineNuxtConfig({
-  devtools: { enabled: true }
+  ssr: false,
+  devtools: { enabled: true },
+  modules: [
+    (_options, nuxt) => {
+      nuxt.hooks.hook('vite:extendConfig', (config) => {
+        config.plugins?.push(
+          vuetify()
+        )
+      })
+    }
+  ],
+  build: {
+    transpile: ['vuetify']
+  },
+  srcDir,
+  vite: {
+    define: {
+      'window.global': {}
+    },
+    // https://stackoverflow.com/questions/70938763/build-problem-with-react-vitejs-and-was-amplify
+    resolve: {
+      alias: {
+        './runtimeConfig': './runtimeConfig.browser'
+      }
+    }
+  },
 })
